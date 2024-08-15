08fe0071a64377259b599d212cfc568326bf662c
Majid Hoseini <majidjj108@gmail.com>
2024-08-15 19:57:20 +0330

    Created card component

--- app/components/AppText.js
+++ app/components/AppText.js
@@ -1,23 +1,14 @@
 import React from "react";
 import { Text, StyleSheet, Platform } from "react-native";
 
-function AppText({ children }) {
-  return <Text style={styles.text}>{children}</Text>;
+function AppText({ children, style }) {
+  return <Text style={[styles.text, style]}>{children}</Text>;
 }
 
 const styles = StyleSheet.create({
   text: {
-    color: "tomato",
-    ...Platform.select({
-      ios: {
-        fontSize: 20,
-        fontFamily: "Avenir",
-      },
-      android: {
-        fontSize: 18,
-        fontFamily: "Roboto",
-      },
-    }),
+    fontSize: 18,
+    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
   },
 });
 
