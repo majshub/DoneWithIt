60c92bedb4a9e8d30eed104c9d66e29c82928453
Majid Hoseini <majidjj108@gmail.com>
2024-08-15 19:09:34 +0330

    Created Button component

--- App.js
+++ App.js
@@ -1,7 +1,21 @@
 import React from "react";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
+import { View } from "react-native";
+import AppText from "./app/components/AppText";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import AppButton from "./app/components/AppButton";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <AppButton title="Login" onPress={() => console.log("tapped")} />
+    </View>
+  );
 }
--- app/config/colors.js
+++ app/config/colors.js
@@ -2,4 +2,5 @@ export default {
   primary: "#fc5c65",
   scondary: "#4ecdc4",
   black: "#000",
+  white: "#fff",
 };
