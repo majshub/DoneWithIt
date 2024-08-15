4a241cd2feb1e72a4a44bc5aabc01060c3b517b8
Majid Hoseini <majidjj108@gmail.com>
2024-08-15 20:37:25 +0330

    Created list item and improved listing details screen

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Image, View, StyleSheet } from "react-native";
 import AppText from "../components/AppText";
+import ListItem from "../components/ListItem";
 
 import colors from "../config/colors";
 
@@ -11,6 +12,13 @@ function ListingDetailsScreen(props) {
       <View style={styles.detailsContainer}>
         <AppText style={styles.title}>Red jacket for sale</AppText>
         <AppText style={styles.price}>$100</AppText>
+        <View style={styles.userContainer}>
+          <ListItem
+            image={require("../assets/mosh.jpg")}
+            title="Mosh Hamedani"
+            subTitle="5 Listings"
+          />
+        </View>
       </View>
     </View>
   );
@@ -33,6 +41,9 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     fontWeight: "500",
+  },
+  userContainer: {
+    marginVertical: 40,
   },
 });
 
