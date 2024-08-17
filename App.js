5e41871a7fe18f73417d765b720d52842a8117d2
Majid Hoseini <majidjj108@gmail.com>
2024-08-17 16:51:52 +0330

    created icon component

--- App.js
+++ App.js
@@ -2,7 +2,13 @@ import React from "react";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
+import Screen from "./app/components/Screen";
+import Icon from "./app/components/Icon";
 
 export default function App() {
-  return <MessagesScreen />;
+  return (
+    <Screen>
+      <Icon name="email" />
+    </Screen>
+  );
 }
