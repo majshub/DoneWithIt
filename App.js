0c612c89a746da399df8e13f0d0b84d935b50c61
Majid Hoseini <majidjj108@gmail.com>
2024-08-17 17:01:38 +0330

    Extented list item component

--- App.js
+++ App.js
@@ -4,11 +4,12 @@ import ViewImageScreen from "./app/screens/ViewImageScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
 import Screen from "./app/components/Screen";
 import Icon from "./app/components/Icon";
+import ListItem from "./app/components/ListItem";
 
 export default function App() {
   return (
     <Screen>
-      <Icon name="email" />
+      <ListItem title="My title" ImageComponent={<Icon name="email" />} />
     </Screen>
   );
 }
