7ab0812ac9e2b91dce1bd9d221663224a01c643e
Majid Hoseini <majidjj108@gmail.com>
2024-08-17 14:12:30 +0330

    Created messages screen

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return <MessagesScreen />;
 }
