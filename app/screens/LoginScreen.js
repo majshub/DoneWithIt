abc0303150c806f2fb090a021dbdec8570ef3dee
Majid Hoseini <majidjj108@gmail.com>
2024-08-19 16:07:57 +0330

    Refactor - Organized form components

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,15 +1,9 @@
 import React from "react";
 import { Image, StyleSheet } from "react-native";
-import Screen from "../components/Screen";
-import { Formik } from "formik";
 import * as Yup from "yup";
 
-import AppButton from "../components/AppButton";
-import ErrorMessage from "../components/ErrorMessage";
-import AppTextInput from "../components/AppTextInput";
-import AppFormField from "../components/AppFormField";
-import SubmitButton from "../components/SubmitButton";
-import AppForm from "../components/AppForm";
+import Screen from "../components/Screen";
+import { AppForm, AppFormField, SubmitButton } from "../components/forms";
 
 const validationSchema = Yup.object().shape({
   email: Yup.string().required().email().label("Email"),
