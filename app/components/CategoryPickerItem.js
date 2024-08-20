b5e73fa3a7f2c29f387d589b87bf6686f14a546c
Majid Hoseini <majidjj108@gmail.com>
2024-08-20 20:45:27 +0330

    Fixked the issue with category picker item

--- app/components/CategoryPickerItem.js
+++ app/components/CategoryPickerItem.js
@@ -1,12 +1,19 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, TouchableOpacity } from "react-native";
+
 import Icon from "./Icon";
 import AppText from "./Text";
 
 function CategoryPickerItem({ item, onPress }) {
   return (
     <View style={styles.container}>
-      <Icon backgroundColor={item.backgroundColor} name={item.icon} size={80} />
+      <TouchableOpacity onPress={onPress}>
+        <Icon
+          backgroundColor={item.backgroundColor}
+          name={item.icon}
+          size={80}
+        />
+      </TouchableOpacity>
       <AppText style={styles.label}>{item.label}</AppText>
     </View>
   );
