c51b0541e6501eed5c32213b0f77496906788c18
Majid Hoseini <majidjj108@gmail.com>
2024-08-21 16:18:30 +0330

    Created feed navigator

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -5,13 +5,15 @@ import ListItem from "../components/lists/ListItem";
 
 import colors from "../config/colors";
 
-function ListingDetailsScreen(props) {
+function ListingDetailsScreen({ route }) {
+  const listing = route.params;
+
   return (
     <View>
-      <Image style={styles.image} source={require("../assets/jacket.jpg")} />
+      <Image style={styles.image} source={listing.image} />
       <View style={styles.detailsContainer}>
-        <AppText style={styles.title}>Red jacket for sale</AppText>
-        <AppText style={styles.price}>$100</AppText>
+        <AppText style={styles.title}>{listing.title}</AppText>
+        <AppText style={styles.price}>${listing.price}</AppText>
         <View style={styles.userContainer}>
           <ListItem
             image={require("../assets/mosh.jpg")}
@@ -44,6 +46,7 @@ const styles = StyleSheet.create({
   },
   userContainer: {
     marginVertical: 40,
+    height: 100,
   },
 });
 
