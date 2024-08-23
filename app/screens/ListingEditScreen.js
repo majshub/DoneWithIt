24787cd57365a22d3a26fe64c740036ceca0332c
Majid Hoseini <majidjj108@gmail.com>
2024-08-23 15:28:36 +0330

    Created upload screen and progress bar

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet } from "react-native";
 import * as Yup from "yup";
 
@@ -13,6 +13,7 @@ import CategoryPickerItem from "../components/CategoryPickerItem";
 import FormImagePicker from "../components/forms/FormImagePicker";
 import listingsApi from "../api/listings";
 import useLocation from "../hooks/useLocation";
+import UploadScreen from "./UploadScreen";
 
 const validationSchema = Yup.object().shape({
   title: Yup.string().required().min(1).label("Title"),
@@ -81,19 +82,26 @@ const categories = [
 
 function ListingEditScreen() {
   const location = useLocation();
+  const [uploadVisible, setUploadVisible] = useState(false);
+  const [progress, setProgress] = useState(0);
 
   const handleSubmit = async (listing) => {
-    const result = await listingsApi.addListing({ ...listing, location });
-    if (!result.ok) {
-      alert("Could not save the listing.");
-      console.log(result.problem);
-      return;
-    }
+    setProgress(0);
+    setUploadVisible(true);
+    const result = await listingsApi.addListing(
+      { ...listing, location },
+      (progress) => setProgress(progress)
+    );
+    setUploadVisible(false);
+
+    if (!result.ok) return alert("Could not save the listing.");
+
     alert("Success");
   };
 
   return (
     <Screen style={styles.container}>
+      <UploadScreen progress={progress} visible={uploadVisible} />
       <Form
         initialValues={{
           title: "",
---
+++ app/screens/UploadScreen.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { View, StyleSheet, Modal } from "react-native";
+import * as Progress from "react-native-progress";
+
+import Text from "../components/Text";
+import colors from "../config/colors";
+
+function UploadScreen({ progress = 0, visible = false }) {
+  return (
+    <Modal visible={visible}>
+      <View style={styles.container}>
+        <Progress.Bar color={colors.primary} progress={progress} width={200} />
+      </View>
+    </Modal>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+  },
+});
+
+export default UploadScreen;
