e95685cdfadbdff8a066be1697fecff3e5af530b
Majid Hoseini <majidjj108@gmail.com>
2024-09-02 15:21:53 +0330

    Connected the app to cloud backend

--- app/config/settings.js
+++ app/config/settings.js
@@ -2,13 +2,13 @@ import Constants from "expo-constants";
 
 const settings = {
   dev: {
-    apiUrl: "http://192.168.100.3:9000/api",
+    apiUrl: "https://donewithit-api-node.onrender.com/api",
   },
   staging: {
-    apiUrl: "http://192.168.100.3:9000/api",
+    apiUrl: "https://donewithit-api-node.onrender.com/api",
   },
   prod: {
-    apiUrl: "http://192.168.100.3:9000/api",
+    apiUrl: "https://donewithit-api-node.onrender.com/api",
   },
 };
 
